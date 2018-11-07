3c29955e0310a9145549caeb0546beb7f2872d97
Janina Kokkonen <janina.kokkonen@gmail.com>
2018-11-07 09:02:27 +0100

    Update started

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,31 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :complited)
+  end
+
+end
